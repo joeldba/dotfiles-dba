5abee938d77f58344437f96ae8ac3cf7f477e48c
joeldba <75224242+joeldba@users.noreply.github.com>
2020-12-08 22:42:53 -0700

    Update deploy-dba.sh

--- deploy-dba.sh
+++ deploy-dba.sh
@@ -16,7 +16,7 @@ echo "installing your programs... answer "y" when necessary"
 sleep 2
 
 # get packages from pacman and aur
-sudo pacman -S xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia ttf-droid terminus-font-otb noto-fonts-emoji picom neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings
+sudo pacman -S xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia ttf-droid terminus-font-otb noto-fonts-emoji ttf-dejavu picom neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings
 
 git clone https://aur.archlinux.org/gtk-theme-arc-gruvbox-git.git
 cd gtk-theme-arc-gruvbox-git

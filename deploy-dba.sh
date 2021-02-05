3f379cedf6a060dfd36df489fa486efceb7a3c75
joeldba <75224242+joeldba@users.noreply.github.com>
2021-02-04 17:11:05 -0700

    Update deploy-dba.sh

--- deploy-dba.sh
+++ deploy-dba.sh
@@ -16,13 +16,7 @@ echo "installing your programs... answer "y" when necessary"
 sleep 2
 
 # get packages from pacman and aur
-sudo pacman -S pacman-contrib xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia ttf-droid noto-fonts-emoji ttf-hack ttf-dejavu neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme adapta-gtk-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings
-
-git clone https://aur.archlinux.org/picom-tryone-git.git
-cd picom-tryone-git
-makepkg -si
-cd ..
-rm -rf picom-tryone-git
+sudo pacman -S zsh opendoas pacman-contrib xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia noto-fonts-emoji ttf-hack ttf-dejavu neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme adapta-gtk-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings zsh-autosuggestions zsh-syntax-highlighting picom
 
 git clone https://aur.archlinux.org/ttf-ms-fonts.git
 cd ttf-ms-fonts
@@ -35,24 +29,6 @@ cd minecraft-launcher
 makepkg -si
 cd ..
 rm -rf minecraft-launcher
-
-git clone https://aur.archlinux.org/pfetch.git
-cd pfetch
-makepkg -si
-cd ..
-rm -rf pfetch
-
-git clone https://aur.archlinux.org/tty-clock.git
-cd tty-clock
-makepkg -si
-cd ..
-rm -rf tty-clock
-
-git clone https://aur.archlinux.org/cli-visualizer.git
-cd cli-visualizer
-makepkg -si
-cd ..
-rm -rf cli-visualizer
 
 echo "your programs have been installed. beginning deployment of suckless utils..."
 sleep 3
@@ -127,4 +103,4 @@ cp -rv .config/Kvantum ~/.config
 cp -rv .config/picom ~/.config
 
 # nothing failed... hopefully ;)
-echo "task failed successfully"
+echo "deployment finished"

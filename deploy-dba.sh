3685ef2efc9b3f03fc79022aa6f075880ed21b3d
joeldba <75224242+joeldba@users.noreply.github.com>
2021-02-04 17:30:17 -0700

    Update deploy-dba.sh

--- deploy-dba.sh
+++ deploy-dba.sh
@@ -16,7 +16,7 @@ echo "installing your programs... answer "y" when necessary"
 sleep 2
 
 # get packages from pacman and aur
-sudo pacman -S zsh opendoas pacman-contrib xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia noto-fonts-emoji ttf-hack ttf-dejavu neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme adapta-gtk-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings zsh-autosuggestions zsh-syntax-highlighting picom
+sudo pacman -S zsh opendoas pacman-contrib xorg-server xorg-xinit libx11 libxcb libxft libxinerama nvidia noto-fonts-emoji ttf-hack ttf-dejavu neofetch ranger cmus htop asciiquarium cmatrix vim nitrogen firefox discord gimp audacity steam virtualbox papirus-icon-theme adapta-gtk-theme lxappearance kvantum-qt5 scrot sxiv zip unzip nvidia-settings zsh-autosuggestions zsh-syntax-highlighting zsh-completions picom
 
 git clone https://aur.archlinux.org/ttf-ms-fonts.git
 cd ttf-ms-fonts
